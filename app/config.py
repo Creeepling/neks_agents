5a2889c35106fa684fd97e4d1e6997006a8e8ebd
Neks Dev <dev@neks.local>
2026-07-10 06:48:22 +0300

    Add Telegram bot configuration

--- app/config.py
+++ app/config.py
@@ -26,6 +26,10 @@ class Settings(BaseSettings):
     
     DADATA_API_KEY: Optional[str] = None
     TWOGIS_API_KEY: Optional[str] = None
+    
+    # --- Telegram ---
+    TELEGRAM_BOT_TOKEN: Optional[str] = "6994180647:AAGHPzOT9P7GE4oaNodX2U5zBB5KgjRw5W4"
+    TELEGRAM_CHAT_ID: Optional[str] = "127932719"
 
     # --- Environment ---
     ENVIRONMENT: str = "local"  # "local" | "production"
